5d3aa2872cdf1518da0a59d733cd8a3cb3ca04ca
Adil Shaikh <adilshaikhhh@outlook.com>
2022-03-05 10:49:28 +0530

    second commit

---
+++ OOPS-DAYS/src/Car.java
@@ -0,0 +1,29 @@
+
+public class Car {
+	
+	int noofcars; 
+	String Model;
+	
+	void display ()
+	{
+		System.out.println("Welcome to Kia-Motors");
+	}
+	
+
+	void display2 ()
+	{
+		System.out.println("Welcome to Benz");
+	}
+	
+
+	void display3 ()
+	{
+		System.out.println("Welcome to Audi");
+	}
+	
+
+	public static void main(String[] args)
+	    {
+		Car obj=new Car();
+		obj.display2();
+		}}
