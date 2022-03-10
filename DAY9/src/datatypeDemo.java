28428c300cdc9b24d756eb19b0a091a6e5dbb443
Adil Shaikh <adilshaikhhh@outlook.com>
2022-03-10 22:21:01 +0530

    Day9 commit

---
+++ DAY9/src/datatypeDemo.java
@@ -0,0 +1,26 @@
+
+public class datatypeDemo {
+	public static void main (String args[])
+	{
+		//int,byte,short,float,double,char,boolean,long
+		int var1 =12;
+		float var2 = 12.2f;
+		double var3 = 12.1;
+		char  var4 = 'A';
+		boolean var5 = true;
+		byte var6 = 127;
+		short var7 = 32767;
+		long var8 = 500L;
+		
+		System.out.println(var1+"\n");
+		System.out.println(var2+"\n");
+		System.out.println(var3+"\n");
+		System.out.println(var4+"\n");
+		System.out.println(var5+"\n");
+		System.out.println(var6+"\n");
+		System.out.println(var7+"\n");
+		System.out.println(var8+"\n");
+	}
+
+
+}
