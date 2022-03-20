09adb2e96eee6e425e40a584795071196a4ba294
Adil Shaikh <adilshaikhhh@outlook.com>
2022-03-20 13:48:06 +0530

    interface and wrapper

---
+++ DAY14/src/wrapper/Demo_wrap.java
@@ -0,0 +1,16 @@
+package wrapper;
+
+public class Demo_wrap {
+
+	public static void main(String[] args) {
+		int i=20; 
+		Integer a=i;
+		Integer j=new Integer (10);
+		System.out.println(+a);
+		System.out.println (+j);
+		System.out.println(+i);
+		
+
+	}
+
+}
