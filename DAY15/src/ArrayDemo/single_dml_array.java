4dddb5a7e9214b8693ef37e28de9289a24f4cf36
Adil Shaikh <adilshaikhhh@outlook.com>
2022-03-21 22:36:30 +0530

    Arrays

---
+++ DAY15/src/ArrayDemo/single_dml_array.java
@@ -0,0 +1,18 @@
+package ArrayDemo;
+
+public class single_dml_array
+{
+	public static void main (String[]args)
+	{
+		int a[]=new int [5];
+		a[0]=34;
+		a[1]=12;
+		a[2]=3;
+		a[3]=78;
+		a[4]=2;
+		for (int i=0;i<a.length;i++)
+		{
+			System.out.println (a[i]);
+			
+		}
+	}}
